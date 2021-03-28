b7cd954118197fa8fdb7c74d9f23758ed7cd4488
Edrick <edrick_42@outlook.com>
2021-03-28 16:08:56 -0300

    Privity ip

--- frontend/src/app/common/const/base-backend-path.const.ts
+++ frontend/src/app/common/const/base-backend-path.const.ts
@@ -1,4 +1,5 @@
-export const BASE_BACKEND_PATH = '/api/';
+export const BASE_BACKEND_PATH = 'http://172.26.9.156:3000/api/';
+//'/api/';
 //'http://localhost:3000/api/' 
 //'http://127.0.0.1:3000/api/';
 //'http://99.79.89.235/api/';
