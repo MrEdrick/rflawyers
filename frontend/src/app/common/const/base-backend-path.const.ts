a1669cc21800da0ca7aed6c5907684ffe8b4e364
Edrick Manoel <edrickmanoel@nasajon.com.br>
2021-12-21 18:10:00 -0300

    Ajust from

--- frontend/src/app/common/const/base-backend-path.const.ts
+++ frontend/src/app/common/const/base-backend-path.const.ts
@@ -1,4 +1,4 @@
-export const BASE_BACKEND_PATH = 'http://localhost:3000/api/ ';
+export const BASE_BACKEND_PATH = 'http://localhost:3000/api/';
 ///api/;
 //https://172.26.9.156:3000/api/
 //http://localhost:3000/api/ 
