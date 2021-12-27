803259e8a87b6207e051aa1faa4b918c82acdd10
Edrick Manoel <edrickmanoel@nasajon.com.br>
2021-12-27 14:49:10 -0300

    Http to Https

--- frontend/src/app/common/const/base-backend-path.const.ts
+++ frontend/src/app/common/const/base-backend-path.const.ts
@@ -1,4 +1,4 @@
-export const BASE_BACKEND_PATH = 'https://99.79.89.235/api/';
+export const BASE_BACKEND_PATH = 'https://99.79.89.235:3000/api/';
 ///api/;
 //https://172.26.9.156:3000/api/
 //http://localhost:3000/api/ 
