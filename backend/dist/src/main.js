af262786d2876cd8dbf0987f51010709add4643e
Edrick Manoel <edrickmanoel@nasajon.com.br>
2021-12-30 18:43:22 -0300

    Enable multiple cors

--- backend/dist/src/main.js
+++ backend/dist/src/main.js
@@ -5,8 +5,11 @@ const app_module_1 = require("./app.module");
 const config = require("config");
 async function bootstrap() {
     const serverConfig = config.get('server');
-    const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    const app = await core_1.NestFactory.create(app_module_1.AppModule, { cors: true });
     app.setGlobalPrefix('api');
+    app.enableCors({
+        origin: '/^(https:\/\/([^\.]*\.)?rodriguesfranceschini\.com)$/i',
+    });
     const port = process.env.PORT || serverConfig.port;
     await app.listen(port);
 }
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,8 +6,12 @@ import * as config from 'config';
 async function bootstrap() {
   const serverConfig = config.get('server');
   
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.setGlobalPrefix('api');
+
+  app.enableCors({
+    origin: '/^(https:\/\/([^\.]*\.)?rodriguesfranceschini\.com)$/i',
+  });
 
   const port = process.env.PORT || serverConfig.port;
   await app.listen(port);
