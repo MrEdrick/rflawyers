643d33e745a62861a4e02e528976c21f06bba949
Edrick Manoel <edrickmanoel@nasajon.com.br>
2021-12-30 12:42:40 -0300

    Enable multiple cors

--- backend/dist/src/main.js
+++ backend/dist/src/main.js
@@ -7,16 +7,8 @@ async function bootstrap() {
     const serverConfig = config.get('server');
     const app = await core_1.NestFactory.create(app_module_1.AppModule, { cors: true });
     app.setGlobalPrefix('api');
-    var whitelist = [serverConfig.origin, serverConfig.domain, serverConfig.domain_www];
     app.enableCors({
-        origin: function (origin, callback) {
-            if (!origin || whitelist.indexOf(origin) !== -1) {
-                callback(null, true);
-            }
-            else {
-                callback(new Error('Not allowed by CORS'));
-            }
-        },
+        origin: true,
         allowedHeaders: 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
         methods: "GET, PUT, POST, DELETE, UPDATE, OPTIONS",
         credentials: true
